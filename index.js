4920f5e3cdccc8120637cd672d66da6c9681e211
Trương Đinh Đăng Khoa <khoatdd0811@gmail.com>
2023-05-15 22:18:56 +0700

    Add Pacman with movement
    draw pacman

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ class Boundary {
 }
 
 
+class Player{
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15        
+    }
+
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'yellow'
+        c.fill();
+        c.closePath();
+    }
+}
+
+
+// create map
 const map = [
     ['-', '-', '-', '-', '-', '-'],
     ['-', ' ', ' ', ' ', ' ', '-'],
@@ -35,9 +53,18 @@ const map = [
 ]
 
 
-const boundaries = [
- 
-]
+const boundaries = []
+
+const player = new Player({
+    position: {
+        x : Boundary.width + Boundary.width / 2 ,
+        y : Boundary.height + Boundary.height / 2
+    },
+    velocity: {
+        x : 0,
+        y : 0
+    }
+})
 
 map.forEach(
     (row, i) => {
@@ -58,4 +85,14 @@ map.forEach(
 
 boundaries.forEach(boundary => {
     boundary.draw()
+})
+
+player.draw()
+
+
+
+// moving
+
+addEventListener('keydown', (event) => {
+console.log(event)
 })
